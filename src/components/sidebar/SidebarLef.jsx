0e48dc62780088171dcef3ee14a587e6ddc8fe97
Jeison Imbana <jeisonimbana@gmail.com>
2022-04-15 23:56:38 -0500

    movie wp 15 version 1.04

--- src/components/sidebar/SidebarLef.jsx
+++ src/components/sidebar/SidebarLef.jsx
@@ -58,7 +58,7 @@ const SidebarLef = () => {
 
    return (
       <div className="sidebarlef">
-         <div className="containermaterial">
+         <div className="containermaterial movil">
             <Autocomplete
                color="primary"
                onChange={(event, newValue) => {
